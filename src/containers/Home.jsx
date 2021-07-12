f444f2761308e851a7ae68b3d90843fbe810e797
David Basto <davidbasto01@gmail.com>
2021-07-12 17:24:27 -0500

    se agregan las rutas con react-router-dom

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,17 +5,15 @@ import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
-import '../assets/styles/App.scss'
+import '../assets/styles/Home.scss'
 import useInitialState from '../hooks/useInitialState'
 
 const API = 'http://localhost:3000/initalState'
 
-const App = () => {
+const Home = () => {
 const initialState = useInitialState(API)
-console.log(initialState);
 return initialState === 0 ? <h1>Loading...</h1>:(
-    <div className="App">
-        <Header />
+    <>
         <Search />
         {initialState.mylist.length > 0 &&
             <Categories title="Mi Lista">
@@ -49,10 +47,8 @@ return initialState === 0 ? <h1>Loading...</h1>:(
             </Carousel>
             </Categories>
         }
-
-        <Footer />
-    </div>
+    </>
 )
 }
 
-export default App
+export default Home
